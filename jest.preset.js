b673f21539c5aaa192404ffed1665ba70c50d8db
Gabriel Sousa Lucas <gsousalucas@outlook.com>
2024-06-05 16:24:42 -0300

    feat: add cobertura

--- jest.preset.js
+++ jest.preset.js
@@ -4,5 +4,5 @@ module.exports = {
   ...nxPreset,
   testTimeout: 10_000,
   testEnvironment: 'node',
-  coverageReporters: [['text', 'html', { skipFull: true }]],
+  coverageReporters: [['text', 'html', 'cobertura', { skipFull: true }]],
 };
