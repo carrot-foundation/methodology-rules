d0a2517c21f933720d94ccfff0784561c6ba6486
Gabriel Lopes <gabrielopes31@gmail.com>
2024-09-13 10:44:06 -0300

    feat: remove old mass-certificate rules [CARROT-1345]

--- apps/methodologies/bold/rule-processors/mass-certificate/mass-validation-document/src/lambda.ts
+++
@@ -1,8 +0,0 @@
-import { wrapRuleIntoLambdaHandler } from '@carrot-fndn/shared/lambda/wrapper';
-
-import { MassAuditDocumentProcessor } from './lib/mass-validation-document.processor';
-
-const instance = new MassAuditDocumentProcessor();
-
-// TODO: we can try to generate this code with a ts-patch program transformer
-export const handler = wrapRuleIntoLambdaHandler(instance);
