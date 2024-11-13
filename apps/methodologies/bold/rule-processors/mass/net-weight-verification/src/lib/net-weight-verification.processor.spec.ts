c04fc747f40e4b820c3efa00fcca9d758551e21b
Cristiano Santos <cristianorsantos.95@gmail.com>
2024-11-13 16:25:47 +0000

    feat(rule): add net weight verification result comments

--- apps/methodologies/bold/rule-processors/mass/net-weight-verification/src/lib/net-weight-verification.processor.spec.ts
+++ apps/methodologies/bold/rule-processors/mass/net-weight-verification/src/lib/net-weight-verification.processor.spec.ts
@@ -15,9 +15,15 @@ import { faker } from '@faker-js/faker';
 import { random } from 'typia';
 
 import { NetWeightVerificationProcessor } from './net-weight-verification.processor';
-import { stubWeighingMoveEvent } from './net-weight-verification.stubs';
+import {
+  APPROVED_WEIGHING_MOVE_EVENT_ATTRIBUTES,
+  stubApprovedWeighingMoveEvent,
+} from './net-weight-verification.stubs';
 
 jest.mock('@carrot-fndn/methodologies/bold/io-helpers');
+
+const { LOAD_NET_WEIGHT, VEHICLE_GROSS_WEIGHT, VEHICLE_WEIGHT } =
+  DocumentEventAttributeName;
 
 describe('NetWeightVerificationProcessor', () => {
   const ruleDataProcessor = new NetWeightVerificationProcessor();
@@ -26,7 +32,10 @@ describe('NetWeightVerificationProcessor', () => {
   it.each([
     {
       document: stubDocument({
-        externalEvents: [stubWeighingMoveEvent()],
+        externalEvents: [
+          stubApprovedWeighingMoveEvent(),
+          stubApprovedWeighingMoveEvent(),
+        ],
       }),
       resultStatus: RuleOutputStatus.APPROVED,
       scenario: 'all the attributes with the correct values',
@@ -39,22 +48,23 @@ describe('NetWeightVerificationProcessor', () => {
     {
       document: stubDocument({
         externalEvents: [
+          stubApprovedWeighingMoveEvent(),
           replaceMetadataAttributeValue(
-            stubWeighingMoveEvent(),
-            DocumentEventAttributeName.LOAD_NET_WEIGHT,
-            '',
+            stubApprovedWeighingMoveEvent(),
+            VEHICLE_GROSS_WEIGHT,
+            `${faker.number.int()}`,
           ),
         ],
       }),
-      resultStatus: RuleOutputStatus.APPROVED,
-      scenario: 'no load-net-weight attribute',
+      resultStatus: RuleOutputStatus.REJECTED,
+      scenario: 'one of the events has invalid attributes',
     },
     {
       document: stubDocument({
         externalEvents: [
           replaceMetadataAttributeValue(
-            stubWeighingMoveEvent(),
-            DocumentEventAttributeName.VEHICLE_GROSS_WEIGHT,
+            stubApprovedWeighingMoveEvent(),
+            VEHICLE_GROSS_WEIGHT,
             '12.00 KG',
           ),
         ],
@@ -63,24 +73,54 @@ describe('NetWeightVerificationProcessor', () => {
       scenario:
         'load-net-weight is not equal to vehicle-gross-weight minus vehicle-weight',
     },
-    ...[
-      DocumentEventAttributeName.VEHICLE_WEIGHT,
-      DocumentEventAttributeName.VEHICLE_GROSS_WEIGHT,
-      DocumentEventAttributeName.LOAD_NET_WEIGHT,
-    ].map((attributeName) => ({
-      document: stubDocument({
-        externalEvents: [
-          replaceMetadataAttributeValue(
-            stubWeighingMoveEvent(),
-            attributeName,
-            `${faker.number.int()}`,
-          ),
-        ],
+    ...[VEHICLE_WEIGHT, VEHICLE_GROSS_WEIGHT, LOAD_NET_WEIGHT].map(
+      (attributeName) => ({
+        document: stubDocument({
+          externalEvents: [
+            replaceMetadataAttributeValue(
+              stubApprovedWeighingMoveEvent(),
+              attributeName,
+              `${faker.number.int()}`,
+            ),
+          ],
+        }),
+        resultStatus: RuleOutputStatus.REJECTED,
+        scenario: `the ${attributeName} attribute is missing the measurement unit`,
       }),
-
-      resultStatus: RuleOutputStatus.REJECTED,
-      scenario: `the ${attributeName} attribute is not in the correct format`,
-    })),
+    ),
+    ...[VEHICLE_WEIGHT, VEHICLE_GROSS_WEIGHT, LOAD_NET_WEIGHT].map(
+      (attributeName) => ({
+        document: stubDocument({
+          externalEvents: [
+            replaceMetadataAttributeValue(
+              stubApprovedWeighingMoveEvent(),
+              attributeName,
+              'undefined KG',
+            ),
+          ],
+        }),
+        resultStatus: RuleOutputStatus.REJECTED,
+        scenario: `the ${attributeName} attribute is not in the correct format`,
+      }),
+    ),
+    ...[VEHICLE_WEIGHT, VEHICLE_GROSS_WEIGHT, LOAD_NET_WEIGHT].map(
+      (attributeName) => ({
+        document: stubDocument({
+          externalEvents: [
+            {
+              ...stubApprovedWeighingMoveEvent(),
+              metadata: {
+                attributes: APPROVED_WEIGHING_MOVE_EVENT_ATTRIBUTES.filter(
+                  ({ name }) => name !== attributeName,
+                ),
+              },
+            },
+          ],
+        }),
+        resultStatus: RuleOutputStatus.APPROVED,
+        scenario: `the ${attributeName} attribute is not present`,
+      }),
+    ),
   ])(
     `should return $resultStatus when the document has $scenario`,
     async ({ document, resultStatus }) => {
@@ -94,9 +134,12 @@ describe('NetWeightVerificationProcessor', () => {
         requestId: ruleInput.requestId,
         responseToken: ruleInput.responseToken,
         responseUrl: ruleInput.responseUrl,
+        resultComment: expect.any(String),
+        resultContent: undefined,
         resultStatus,
       };
 
+      expect(ruleOutput.resultComment).toMatchSnapshot();
       expect(ruleOutput).toEqual(expectedRuleOutput);
     },
   );
--- apps/methodologies/bold/rule-processors/mass/net-weight-verification/src/lib/net-weight-verification.processor.ts
+++ apps/methodologies/bold/rule-processors/mass/net-weight-verification/src/lib/net-weight-verification.processor.ts
@@ -5,6 +5,7 @@ import {
   metadataAttributeValueIsAnyOf,
 } from '@carrot-fndn/methodologies/bold/predicates';
 import {
+  type DocumentEvent,
   DocumentEventAttributeName,
   DocumentEventMoveType,
   DocumentEventName,
@@ -19,9 +20,143 @@ import {
   RuleOutputStatus,
 } from '@carrot-fndn/shared/rule/types';
 
-import { extractWeightAttributes } from './net-weight-verification.utils';
+import {
+  extractWeighingAttributes,
+  getWeighingAttributesMissingFields,
+  getWeighingAttributesValidationErrors,
+  isValidWeighingAttributes,
+} from './net-weight-verification.utils';
+
+const { MOVE } = DocumentEventName;
+const { WEIGHING } = DocumentEventMoveType;
+const { MOVE_TYPE } = DocumentEventAttributeName;
+const { APPROVED, REJECTED } = RuleOutputStatus;
+const { LOAD_NET_WEIGHT, VEHICLE_GROSS_WEIGHT, VEHICLE_WEIGHT } =
+  DocumentEventAttributeName;
+
+interface CalculationValues {
+  netWeightResult: number;
+  vehicleGrossWeight: number;
+  vehicleWeight: number;
+}
 
 export class NetWeightVerificationProcessor extends RuleDataProcessor {
+  private ResultComment = {
+    WEIGHING_EVENTS_NOT_FOUND: `${WEIGHING} ${MOVE} events not found`,
+    approved: ({
+      eventIndex,
+      ...weighingValues
+    }: {
+      eventIndex: number;
+    } & CalculationValues) =>
+      `${this.getEventCommentText(eventIndex)}: ${this.formatCalculationText(weighingValues)}`,
+
+    missingValues: ({
+      eventIndex,
+      missingValues,
+    }: {
+      eventIndex: number;
+      missingValues: string;
+    }) =>
+      `${this.getEventCommentText(eventIndex)} is missing attributes: ${missingValues}`,
+
+    rejected: ({
+      eventIndex,
+      loadNetWeight,
+      ...weighingValues
+    }: {
+      eventIndex: number;
+      loadNetWeight: number;
+    } & CalculationValues) =>
+      `${this.getEventCommentText(eventIndex)}: Invalid load net weight (${this.formatCalculationText(weighingValues)}) should be equal to ${loadNetWeight}`,
+
+    validationError: ({
+      eventIndex,
+      validationErrors,
+    }: {
+      eventIndex: number;
+      validationErrors: string;
+    }) =>
+      `${this.getEventCommentText(eventIndex)} has invalid attributes: ${validationErrors}`,
+  };
+
+  private formatCalculationText = ({
+    netWeightResult,
+    vehicleGrossWeight,
+    vehicleWeight,
+  }: CalculationValues): string =>
+    `${vehicleGrossWeight} - ${vehicleWeight} = ${netWeightResult}`;
+
+  private getEventCommentText = (eventIndex: number): string =>
+    `${WEIGHING} ${MOVE} event (${eventIndex + 1})`;
+
+  private validateWeighingEvents(weighingEvents: DocumentEvent[]) {
+    const rejectedResultComments: string[] = [];
+    const approvedResultComments: string[] = [];
+
+    for (const [eventIndex, event] of weighingEvents.entries()) {
+      const attributes = extractWeighingAttributes(event);
+      const attributesValues = Object.fromEntries(
+        attributes.map(([key, { value }]) => [key, value]),
+      );
+
+      if (isValidWeighingAttributes(attributesValues)) {
+        const {
+          [LOAD_NET_WEIGHT]: loadNetWeight,
+          [VEHICLE_GROSS_WEIGHT]: vehicleGrossWeight,
+          [VEHICLE_WEIGHT]: vehicleWeight,
+        } = attributesValues;
+        const netWeightResult = vehicleGrossWeight - vehicleWeight;
+
+        if (netWeightResult === loadNetWeight) {
+          approvedResultComments.push(
+            this.ResultComment.approved({
+              eventIndex,
+              netWeightResult,
+              vehicleGrossWeight,
+              vehicleWeight,
+            }),
+          );
+        } else {
+          rejectedResultComments.push(
+            this.ResultComment.rejected({
+              eventIndex,
+              loadNetWeight,
+              netWeightResult,
+              vehicleGrossWeight,
+              vehicleWeight,
+            }),
+          );
+        }
+      } else {
+        const validationErrors =
+          getWeighingAttributesValidationErrors(attributes);
+        const missingValues = getWeighingAttributesMissingFields(attributes);
+
+        if (validationErrors.length > 0) {
+          rejectedResultComments.push(
+            this.ResultComment.validationError({
+              eventIndex,
+              validationErrors,
+            }),
+          );
+        } else if (missingValues.length > 0) {
+          approvedResultComments.push(
+            this.ResultComment.missingValues({
+              eventIndex,
+              missingValues,
+            }),
+          );
+        }
+      }
+    }
+
+    return {
+      approvedResultComments,
+      rejectedResultComments,
+    };
+  }
+
   async process(ruleInput: RuleInput): Promise<RuleOutput> {
     const document = await loadParentDocument(
       this.context.documentLoaderService,
@@ -32,36 +167,33 @@ export class NetWeightVerificationProcessor extends RuleDataProcessor {
     );
 
     if (!document) {
-      return mapToRuleOutput(ruleInput, RuleOutputStatus.REJECTED, {
+      return mapToRuleOutput(ruleInput, REJECTED, {
         resultComment: DOCUMENT_NOT_FOUND_RESULT_COMMENT,
       });
     }
 
-    const { MOVE } = DocumentEventName;
-    const { WEIGHING } = DocumentEventMoveType;
-    const { MOVE_TYPE } = DocumentEventAttributeName;
-
-    const events = (document.externalEvents ?? []).filter(
+    const weighingEvents = (document.externalEvents ?? []).filter(
       and(
         eventNameIsAnyOf([MOVE]),
         metadataAttributeValueIsAnyOf(MOVE_TYPE, [WEIGHING]),
       ),
     );
 
-    const resultStatus = events.every((event) => {
-      const attributes = extractWeightAttributes(event);
+    if (weighingEvents.length === 0) {
+      return mapToRuleOutput(ruleInput, APPROVED, {
+        resultComment: this.ResultComment.WEIGHING_EVENTS_NOT_FOUND,
+      });
+    }
 
-      if (!attributes) {
-        return true;
-      }
+    const { approvedResultComments, rejectedResultComments } =
+      this.validateWeighingEvents(weighingEvents);
 
-      const { loadNetWeight, vehicleGrossWeight, vehicleWeight } = attributes;
-
-      return loadNetWeight === vehicleGrossWeight - vehicleWeight;
-    })
-      ? RuleOutputStatus.APPROVED
-      : RuleOutputStatus.REJECTED;
-
-    return mapToRuleOutput(ruleInput, resultStatus);
+    return rejectedResultComments.length > 0
+      ? mapToRuleOutput(ruleInput, REJECTED, {
+          resultComment: rejectedResultComments.join(' | '),
+        })
+      : mapToRuleOutput(ruleInput, APPROVED, {
+          resultComment: approvedResultComments.join(' | '),
+        });
   }
 }
