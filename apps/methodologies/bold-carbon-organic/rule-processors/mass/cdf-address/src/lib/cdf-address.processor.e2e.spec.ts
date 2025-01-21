0c75f0a4ce3825d2cc97071795cd3446f7cfe11a
AMarcosCastelo <marcos-12marcos@hotmail.com>
2025-01-21 18:51:39 -0300

    chore(rule): create bold-carbon base

---
+++ apps/methodologies/bold-carbon-organic/rule-processors/mass/cdf-address/src/lib/cdf-address.processor.e2e.spec.ts
@@ -0,0 +1,8 @@
+import { testRuleProcessorWithMassDocuments } from '@carrot-fndn/methodologies/bold/recycling/organic/testing';
+
+import { handler } from '../lambda';
+
+testRuleProcessorWithMassDocuments({
+  handler,
+  ruleName: 'CdfAddressProcessor',
+});
