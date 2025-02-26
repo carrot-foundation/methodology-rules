48e56ec5872ebed962b39454163ad7935bdebab1
AMarcosCastelo <marcos-12marcos@hotmail.com>
2025-02-26 14:36:24 -0300

    chore: remove temporary log and TODO comments from lambda handlers

--- apps/methodologies/bold-carbon-organic/rule-processors/mass-id/project-period/src/lambda.ts
+++ apps/methodologies/bold-carbon-organic/rule-processors/mass-id/project-period/src/lambda.ts
@@ -3,5 +3,4 @@ import { wrapRuleIntoLambdaHandler } from '@carrot-fndn/shared/lambda/wrapper';
 
 const instance = new ProjectPeriodProcessor();
 
-// TODO: we can try to generate this code with a ts-patch program transformer
 export const handler = wrapRuleIntoLambdaHandler(instance);
