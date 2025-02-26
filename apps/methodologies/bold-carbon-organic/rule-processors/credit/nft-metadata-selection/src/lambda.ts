28b145b36c81ef73ff6f14a43bfe975fa6a78a1b
AMarcosCastelo <marcos-12marcos@hotmail.com>
2025-02-26 14:33:01 -0300

    refactor(rule): rename the scope Mass to MassID

--- apps/methodologies/bold-carbon-organic/rule-processors/credit/nft-metadata-selection/src/lambda.ts
+++
@@ -1,7 +0,0 @@
-import { wrapRuleIntoLambdaHandler } from '@carrot-fndn/shared/lambda/wrapper';
-
-import { NftMetadataSelection } from './lib';
-
-const instance = new NftMetadataSelection();
-
-export const handler = wrapRuleIntoLambdaHandler(instance);
--- apps/methodologies/bold-carbon-organic/rule-processors/credit/nft-metadata-selection/src/lib/nft-metadata-selection.helpers.typia.ts
+++
@@ -1,6 +0,0 @@
-import { createAssert } from 'typia';
-
-import type { MethodologyCreditNftMetadataDto } from './nft-metadata-selection.dto';
-
-export const assertMethodologyCreditNftMetadataDto =
-  createAssert<MethodologyCreditNftMetadataDto>();
