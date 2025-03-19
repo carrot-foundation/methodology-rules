854093d514f106094e84fac29fd8a8fb15782836
AMarcosCastelo <marcos-12marcos@hotmail.com>
2025-03-19 09:48:16 -0300

    fix(rule): remove unused error message from LocalWasteClassificationProcessorErrors

--- libs/methodologies/bold/rule-processors/mass-id/src/local-waste-classification/local-waste-classification.errors.ts
+++ libs/methodologies/bold/rule-processors/mass-id/src/local-waste-classification/local-waste-classification.errors.ts
@@ -2,7 +2,6 @@ import { BaseProcessorErrors } from '@carrot-fndn/shared/methodologies/bold/proc
 
 export class LocalWasteClassificationProcessorErrors extends BaseProcessorErrors {
   override readonly ERROR_MESSAGE = {
-    DOCUMENT_TYPE_NOT_FOUND: 'Document type not found.',
     REJECTED_BY_ERROR: 'Unable to validate the local-waste-classification.',
   } as const;
 }
