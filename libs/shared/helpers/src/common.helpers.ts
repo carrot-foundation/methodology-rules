16b47d5ff7037155e0c4ea2cb81e4009e0b37ff3
AMarcosCastelo <marcos-12marcos@hotmail.com>
2025-03-26 19:08:16 -0300

    feat(shared): add utility functions for object type checking

--- libs/shared/helpers/src/common.helpers.ts
+++ libs/shared/helpers/src/common.helpers.ts
@@ -3,6 +3,37 @@ import type { NonNullableOrNullOrUndefined } from '@carrot-fndn/shared/types';
 export const isNil = (value: unknown): value is null | undefined =>
   value === null || value === undefined;
 
+export const isObject = (value: unknown): value is Record<string, unknown> =>
+  typeof value === 'object' && value !== null;
+
+export const isPlainObject = (
+  value: unknown,
+): value is Record<string, unknown> => {
+  if (!isObject(value)) {
+    return false;
+  }
+
+  const proto = Object.getPrototypeOf(value) as Record<string, unknown> | null;
+
+  if (proto === null) {
+    return true;
+  }
+
+  const constructor =
+    Object.prototype.hasOwnProperty.call(proto, 'constructor') &&
+    proto.constructor;
+
+  return (
+    typeof constructor === 'function' &&
+    constructor.toString() === Object.toString()
+  );
+};
+
+export const isNonEmptyObject = (
+  value: unknown,
+): value is Record<string, unknown> =>
+  isPlainObject(value) && Object.keys(value).length > 0;
+
 export const pick = <T, K extends keyof T>(
   object: T,
   ...keys: K[]
