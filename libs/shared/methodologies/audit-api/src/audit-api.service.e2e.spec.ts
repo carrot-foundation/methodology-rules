92873cbf71c798412c6d7fcb4074765014d8bb80
AMarcosCastelo <marcos-12marcos@hotmail.com>
2025-03-31 10:11:23 -0300

    refactor(shared): reorganize and enhance checkDuplicateDocuments tests for clarity and structure

--- libs/shared/methodologies/audit-api/src/audit-api.service.e2e.spec.ts
+++ libs/shared/methodologies/audit-api/src/audit-api.service.e2e.spec.ts
@@ -15,47 +15,49 @@ describe('Audit API Service', () => {
     auditApiService = new AuditApiService();
   });
 
-  it('should return a empty array when no document is duplicated', async () => {
-    const response = await auditApiService.checkDuplicateDocuments({
-      match: {
+  describe('checkDuplicateDocuments', () => {
+    it('should return a empty array when no document is duplicated', async () => {
+      const response = await auditApiService.checkDuplicateDocuments({
+        match: {
+          category: faker.string.uuid(),
+          currentValue: faker.number.float(),
+          subType: faker.string.uuid(),
+          type: faker.string.uuid(),
+        },
+      });
+
+      expect(response).toEqual([]);
+    });
+
+    it('should return a list of duplicated documents', async () => {
+      const duplicatedDto = {
         category: faker.string.uuid(),
         currentValue: faker.number.float(),
         subType: faker.string.uuid(),
         type: faker.string.uuid(),
-      },
+      };
+
+      const [documentId1, documentId2] = await Promise.all([
+        seedDocument({
+          endpoint,
+          partialDocument: duplicatedDto,
+        }),
+        seedDocument({
+          endpoint,
+          partialDocument: duplicatedDto,
+        }),
+      ]);
+
+      const response = await auditApiService.checkDuplicateDocuments({
+        match: {
+          ...duplicatedDto,
+        },
+      });
+
+      expect(response).toEqual([
+        { id: documentId2, status: 'OPEN' },
+        { id: documentId1, status: 'OPEN' },
+      ]);
     });
-
-    expect(response).toEqual([]);
-  });
-
-  it('should return a list of duplicated documents', async () => {
-    const duplicatedDto = {
-      category: faker.string.uuid(),
-      currentValue: faker.number.float(),
-      subType: faker.string.uuid(),
-      type: faker.string.uuid(),
-    };
-
-    const [documentId1, documentId2] = await Promise.all([
-      seedDocument({
-        endpoint,
-        partialDocument: duplicatedDto,
-      }),
-      seedDocument({
-        endpoint,
-        partialDocument: duplicatedDto,
-      }),
-    ]);
-
-    const response = await auditApiService.checkDuplicateDocuments({
-      match: {
-        ...duplicatedDto,
-      },
-    });
-
-    expect(response).toEqual([
-      { id: documentId2, status: 'OPEN' },
-      { id: documentId1, status: 'OPEN' },
-    ]);
   });
 });
