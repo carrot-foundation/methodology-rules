50a1ed76f23ef6f3607f717d9bca53afccaefe77
AMarcosCastelo <marcos-12marcos@hotmail.com>
2025-03-31 10:08:52 -0300

    feat(shared): update AUDIT_API_URL handling and add AUDIT_URL environment variable

--- libs/shared/methodologies/audit-api/src/audit-api.constants.ts
+++ libs/shared/methodologies/audit-api/src/audit-api.constants.ts
@@ -1,14 +1,5 @@
-import { getOrDefault } from '@carrot-fndn/shared/helpers';
+import type { Uri } from '@carrot-fndn/shared/types';
+
 import { assert } from 'typia';
 
-export const AUDIT_API_URL_MAP = {
-  development: 'https://smaug.carrot.eco',
-  production: 'https://core.carrot.eco',
-} as const;
-
-export const AUDIT_API_URL =
-  AUDIT_API_URL_MAP[
-    assert<'development' | 'production'>(
-      getOrDefault(process.env['ENVIRONMENT'], 'development'),
-    )
-  ];
+export const AUDIT_API_URL = assert<Uri>(process.env['AUDIT_URL']);
