55b268da2ed3531d7c626082c0728f834e94ce27
AMarcosCastelo <marcos-12marcos@hotmail.com>
2025-04-02 14:47:31 -0300

    refactor(rule): update error messages in uniqueness check for improved clarity and consistency

--- libs/methodologies/bold/rule-processors/mass-id/src/uniqueness-check/uniqueness-check.errors.ts
+++ libs/methodologies/bold/rule-processors/mass-id/src/uniqueness-check/uniqueness-check.errors.ts
@@ -1,22 +1,22 @@
 import { BaseProcessorErrors } from '@carrot-fndn/shared/methodologies/bold/processors';
 import {
-  DocumentCategory,
   DocumentEventName,
+  NewDocumentEventAttributeName,
 } from '@carrot-fndn/shared/methodologies/bold/types';
 import { MethodologyDocumentEventLabel } from '@carrot-fndn/shared/types';
 
 const { DROP_OFF, PICK_UP } = DocumentEventName;
+const { VEHICLE_LICENSE_PLATE } = NewDocumentEventAttributeName;
 const { RECYCLER, WASTE_GENERATOR } = MethodologyDocumentEventLabel;
-const { MASS_ID } = DocumentCategory;
 
 export class UniquenessCheckProcessorErrors extends BaseProcessorErrors {
   override readonly ERROR_MESSAGE = {
     MASS_ID_DOCUMENT_NOT_FOUND: 'MassID document not found.',
-    MISSING_DROP_OFF_EVENT: `Unable to validate the "${MASS_ID}" uniqueness because the "${DROP_OFF}" event is missing.`,
-    MISSING_PICK_UP_EVENT: `Unable to validate the "${MASS_ID}" uniqueness because the "${PICK_UP}" event is missing.`,
-    MISSING_RECYCLER_EVENT: `Unable to validate the "${MASS_ID}" uniqueness because the "${RECYCLER}" event is missing.`,
-    MISSING_VEHICLE_LICENSE_PLATE: `Unable to validate the "${MASS_ID}" uniqueness because the vehicle license plate is missing.`,
-    MISSING_WASTE_GENERATOR_EVENT: `Unable to validate the "${MASS_ID}" uniqueness because the "${WASTE_GENERATOR}" event is missing.`,
+    MISSING_DROP_OFF_EVENT: `Expected "${DROP_OFF}" event.`,
+    MISSING_PICK_UP_EVENT: `Expected "${PICK_UP}" event.`,
+    MISSING_RECYCLER_EVENT: `Expected "${RECYCLER}" event.`,
+    MISSING_VEHICLE_LICENSE_PLATE: `Expected "${VEHICLE_LICENSE_PLATE}" attribute.`,
+    MISSING_WASTE_GENERATOR_EVENT: `Expected "${WASTE_GENERATOR}" event.`,
     REJECTED_BY_ERROR: 'Unable to validate the MassID uniqueness.',
   } as const;
 }
