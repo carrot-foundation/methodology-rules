91a9216187866429a2d7f94a9d95a73160323778
AMarcosCastelo <marcos-12marcos@hotmail.com>
2025-05-29 18:24:37 -0300

    chore: add temporary log for debugging purposes

--- apps/methodologies/bold-carbon/rule-processors/mass-id/certificate-uniquiness-check/src/lambda.ts
+++ apps/methodologies/bold-carbon/rule-processors/mass-id/certificate-uniquiness-check/src/lambda.ts
@@ -2,6 +2,8 @@ import { GAS_ID } from '@carrot-fndn/shared/methodologies/bold/matchers';
 import { certificateUniquenessCheckLambda } from '@carrot-fndn/shared/methodologies/bold/rule-processors/mass-id/certificate-uniqueness-check';
 import { BoldMethodologySlug } from '@carrot-fndn/shared/methodologies/bold/types';
 
+console.log('Temporary log');
+
 export const handler = certificateUniquenessCheckLambda(
   GAS_ID,
   BoldMethodologySlug.CARBON,
