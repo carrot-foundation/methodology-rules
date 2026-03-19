aaddb2193b93947501267c3e0150e4dfa544511e
Cristiano Santos <cristianorsantos.95@gmail.com>
2026-03-19 12:48:07 +0000

    refactor: replace all jest.config.ts with vitest.config.ts

---
+++ libs/shared/methodologies/bold/testing/vitest.config.ts
@@ -0,0 +1,7 @@
+import { getVitestBaseConfig } from '../../../../../.vitest/config/vitest.base.config';
+import { getVitestBasePlugins } from '../../../../../.vitest/config/vitest.base.plugins';
+
+export default {
+  ...getVitestBaseConfig(import.meta.dirname),
+  plugins: getVitestBasePlugins({ dirname: import.meta.dirname }),
+};
--- tools/rule-runner-cli/jest.config.ts
+++
@@ -1,5 +0,0 @@
-import { getJestBaseConfig } from '../../.jest/config/jest.base.config';
-
-const config = getJestBaseConfig(__dirname);
-
-export default config;
